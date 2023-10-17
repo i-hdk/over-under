10462e412f7ab2c2909f12f1a51173aae710c4e6
Isabella Lin <59080145+i-hdk@users.noreply.github.com>
2023-10-18 03:17:45 +0800

    basic driver control template

--- overunder/src/main.cpp
+++ overunder/src/main.cpp
@@ -1,20 +1,16 @@
 #include "main.h"
+#include "api.h"
+#include <math.h>
+#include <string>
+#include <bits/stdc++.h>
+using namespace okapi;
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::Motor backL (12, pros::E_MOTOR_GEARSET_06, 1, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor backR (2, pros::E_MOTOR_GEARSET_06, 0, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor frontL (11, pros::E_MOTOR_GEARSET_06, 1, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor frontR (1, pros::E_MOTOR_GEARSET_06, 0, pros::E_MOTOR_ENCODER_ROTATIONS); 
+pros::Motor roller (16, pros::E_MOTOR_GEARSET_18, 0, pros::E_MOTOR_ENCODER_ROTATIONS);
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -25,8 +21,6 @@ void on_center_button() {
 void initialize() {
 	pros::lcd::initialize();
 	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
 }
 
 /**
@@ -74,19 +68,24 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
-
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
 		int left = master.get_analog(ANALOG_LEFT_Y);
 		int right = master.get_analog(ANALOG_RIGHT_Y);
+		backL = left;
+		frontL = left;
+		backR = right;
+		frontR = right;
 
-		left_mtr = left;
-		right_mtr = right;
+		if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)==1){
+			roller.move_velocity(200);
+		}
+		else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)==1){
+			roller.move_velocity(-200);
+		}
+		else{
+			roller.move_velocity(0);
+		}
+
 
 		pros::delay(20);
 	}
